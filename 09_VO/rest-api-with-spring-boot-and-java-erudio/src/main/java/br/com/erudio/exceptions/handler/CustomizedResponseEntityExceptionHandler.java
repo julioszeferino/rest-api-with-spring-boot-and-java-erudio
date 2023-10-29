20534ae8dbc9bdc09e5c7cae12fbac01d1a49766
Julio Zeferino <julioszeferino@gmail.com>
2023-10-29 18:20:16 -0300

    Value Object Pattern

---
+++ 09_VO/rest-api-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,51 @@
+package br.com.erudio.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.erudio.exceptions.ExceptionResponse;
+import br.com.erudio.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice // permite espalhar o comportamento para todos os controllers
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	// metodo para suportar excecoes genericas direcionando erro 500
+	// A excecao Exception.class e a excecao generica do JAVA
+	@ExceptionHandler(Exception.class)
+	private final ResponseEntity<ExceptionResponse> handleAllExceptions(
+		Exception ex, // excecao
+		WebRequest request // requisicao que gerou a excecao
+	){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+												new Date(),
+												ex.getMessage(),
+												request.getDescription(false)
+											);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	// metodo para suportar nossa excecao especifica
+	@ExceptionHandler(ResourceNotFoundException.class)
+	private final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
+		Exception ex, // excecao
+		WebRequest request // requisicao que gerou a excecao
+	){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+												new Date(),
+												ex.getMessage(),
+												request.getDescription(false)
+											);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
